5e6bd37f110a40bc66c4275ee1d4e1e55ff59e88
Jake Barnby <jakeb994@gmail.com>
2017-07-28 13:45:38 +1200

    Add firebase push notifications. Add firebase crash reporting. Update google-services to accomodate.

---
+++ app/src/main/java/com/jakebarnby/filemanager/services/FirebaseNotificationService.java
@@ -0,0 +1,65 @@
+package com.jakebarnby.filemanager.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.jakebarnby.filemanager.R;
+import com.jakebarnby.filemanager.activities.source.SourceActivity;
+
+/**
+ * Created by jakebarnby on 28/07/17.
+ */
+
+public class FirebaseNotificationService extends FirebaseMessagingService {
+    public static final String TAG = "FIREBASE";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        
+
+        if (remoteMessage.getNotification() != null) {
+            sendNotification(
+                    remoteMessage.getNotification().getTitle(),
+                    remoteMessage.getNotification().getBody()
+            );
+        }
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param title         The title for the notification
+     * @param messageBody   The body for the notification
+     */
+    private void sendNotification(String title, String messageBody) {
+        Intent intent = new Intent(this, SourceActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
